7d368b22de4b60828914a5acf243081bfcef71d2
peace0phmind <peace0phmind@gmail.com>
2015-01-17 16:55:38 +0800

    add defer use performance

--- sync/map_test.go
+++ sync/map_test.go
@@ -72,8 +72,16 @@ func sum(i, j int) int {
 func TestGetOrElse(t *testing.T) {
 	m := Map{}
 
-	if v := m.GetOrElse("222", func() (interface{}, error) { return sum(1, 2), nil }, 100); v != 3 {
+	if v := m.Get(k); v != nil {
+		t.Errorf("want 0, but %v", v)
+	}
+
+	if v := m.GetOrElse(k, func() (interface{}, error) { return sum(1, 2), nil }, 100); v != 3 {
 		t.Errorf("want 3, but get: %v", v)
+	}
+
+	if v := m.Get(k); v != 3 {
+		t.Errorf("want 3, but %v", v)
 	}
 }
 
@@ -156,7 +164,6 @@ func BenchmarkHardCodeInfKey(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		sum += m.Get(k)
 	}
-
 }
 
 type hardCodeInfValMap struct {
@@ -180,5 +187,27 @@ func BenchmarkHardCodeInfVal(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		sum += m.Get(k).(int)
 	}
+}
 
+type hardCodeMapUseDefer struct {
+	sync.RWMutex
+	m map[interface{}]interface{}
+}
+
+func (h *hardCodeMapUseDefer) Get(key interface{}) (v interface{}) {
+	h.RLock()
+	v = h.m[key]
+	defer h.RUnlock()
+	return
+}
+
+func BenchmarkHardCodeDefer(b *testing.B) {
+	m := hardCodeMapUseDefer{}
+	m.m = map[interface{}]interface{}{}
+	m.m[k] = 1
+
+	var sum int
+	for i := 0; i < b.N; i++ {
+		sum += m.Get(k).(int)
+	}
 }
