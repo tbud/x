3c0b467883783e2a0e035868268dc8ff8b3f50fc
peace0phmind <peace0phmind@gmail.com>
2015-01-18 23:43:29 +0800

    add meta

--- meta/meta.go
+++ meta/meta.go
@@ -1,9 +1,11 @@
 package meta
 
 import (
-	"fmt"
 	"reflect"
 	"runtime"
+	"strings"
+	"sync"
+	"unicode"
 )
 
 type MetaInfo struct {
@@ -12,7 +14,7 @@ type MetaInfo struct {
 	Max           int
 	Skip          bool
 	OmitEmpty     bool
-	WithQuote     bool
+	Quote         bool
 	MatchRegExp   string
 	SerializeType string
 }
@@ -24,31 +26,91 @@ const (
 	validateTag = "validate"
 )
 
-const (
-	minFlag   = "("
-	minEqual  = "["
-	sizeSplit = ".."
-	maxFlag   = ")"
-	maxEqual  = "]"
-
-	nonSkipFlag = "+"
-
-	omitEmpty    = "~"
-	nonOmitEmpty = "="
-)
-
-const (
-	skipFlag = iota
-	omitEmptyFlag
-)
-
-var MetaSymbols = map[string]int{
-	"-":         skipFlag,
-	"~":         omitEmptyFlag,
-	"omitempty": omitEmptyFlag,
+func JsonMeta(t reflect.Type) ([]MetaInfo, error) {
+	return meta(t, jsonTag)
 }
 
-func Meta(v reflect.Value, tagName string) (m []MetaInfo, err error) {
+func OrmMeta(t reflect.Type) ([]MetaInfo, error) {
+	return meta(t, ormTag)
+}
+
+func ValidateMeta(t reflect.Type) ([]MetaInfo, error) {
+	return meta(t, validateTag)
+}
+
+type metaCache struct {
+	sync.RWMutex
+	m map[reflect.Type][]MetaInfo
+}
+
+func (m metaCache) getOrElse(key reflect.Type, f func() []MetaInfo) []MetaInfo {
+	m.RLock()
+	v, ok := m.m[key]
+	m.RUnlock()
+	if ok {
+		return v
+	}
+
+	if f == nil {
+		return nil
+	}
+
+	v = f()
+	if v == nil {
+		v = []MetaInfo{}
+	}
+
+	m.Lock()
+	if m.m == nil {
+		m.m = map[reflect.Type][]MetaInfo{}
+	}
+	m.m[key] = v
+	m.Unlock()
+
+	return v
+}
+
+var metaCaches = map[string]metaCache{
+	metaTag:     metaCache{},
+	jsonTag:     metaCache{},
+	ormTag:      metaCache{},
+	validateTag: metaCache{},
+}
+
+func originName(name string) string {
+	return name
+}
+
+func firstLittleName(name string) string {
+	n := []rune(name)
+	n[0] = unicode.ToLower(n[0])
+	return string(n)
+}
+
+func underscoreLittleName(name string) string {
+	r := []rune{}
+	for _, s := range name {
+		if unicode.IsUpper(s) {
+			if len(r) > 0 {
+				r = append(r, []rune("_")[0])
+			}
+
+			r = append(r, unicode.ToLower(s))
+		} else {
+			r = append(r, s)
+		}
+	}
+
+	return string(r)
+}
+
+var nameStrategy = map[string]func(string) string{
+	jsonTag:     firstLittleName,
+	ormTag:      underscoreLittleName,
+	validateTag: originName,
+}
+
+func meta(t reflect.Type, tagName string) (retMeta []MetaInfo, err error) {
 	defer func() {
 		if r := recover(); r != nil {
 			if _, ok := r.(runtime.Error); ok {
@@ -58,27 +120,46 @@ func Meta(v reflect.Value, tagName string) (m []MetaInfo, err error) {
 		}
 	}()
 
-	m = meta(v, tagName)
+	retMeta = metaCaches[tagName].getOrElse(t, func() []MetaInfo {
+		ms := metaCaches[metaTag].getOrElse(t, func() []MetaInfo {
+			metaInfos := make([]MetaInfo, metaTp.NumField())
+			metaFromTag(t, metaTag, metaInfos)
+			return metaInfos
+		})
+
+		metaFromTag(t, tagName, ms)
+
+		for i := 0; i < t.NumField(); i++ {
+			if len(ms[i].Name) == 0 {
+				ms[i].Name = nameStrategy[tagName](t.Field(i).Name)
+			}
+		}
+
+		return ms
+	})
+
 	return
 }
 
-func JsonMeta(v reflect.Value) ([]MetaInfo, error) {
-	return Meta(v, ormTag)
-}
-
-func OrmMeta(v reflect.Value) ([]MetaInfo, error) {
-	return Meta(v, ormTag)
-}
-
-func ValidateMeta(v reflect.Value) ([]MetaInfo, error) {
-	return Meta(v, validateTag)
-}
-
-func meta(v reflect.Value, tagName string) []MetaInfo {
-	t := v.Type()
+func metaFromTag(t reflect.Type, tagName string, metaInfos []MetaInfo) {
 	for i := 0; i < t.NumField(); i++ {
-		fmt.Println(t.Field(i).Tag.Get(tagName))
-	}
+		field := t.Field(i)
+		tag := field.Tag.Get(tagName)
+		meta := &metaInfos[i]
 
-	return []MetaInfo{}
+		if len(tag) > 0 {
+			for _, v := range strings.Split(tag, ",") {
+				switch {
+				case v == "-":
+					meta.Skip = true
+				case v == "~" || v == "omitempty":
+					meta.OmitEmpty = true
+				case v == "string" || v == "%q":
+					meta.Quote = true
+				case unicode.IsLetter([]rune(v)[0]):
+					meta.Name = v
+				}
+			}
+		}
+	}
 }
---
+++ meta/meta_test.go
@@ -0,0 +1,33 @@
+package meta
+
+import (
+	"reflect"
+	"testing"
+)
+
+type MetaInfoTest struct {
+	MetaTag  int    `@:"metaSymbol,123,441"`
+	JsonTag  string `@:"fdsa",json:"tt"`
+	EmptyTag string
+}
+
+var metaTp = reflect.TypeOf(MetaInfoTest{})
+
+func TestJsonMeta(t *testing.T) {
+	mi, ok := JsonMeta(metaTp)
+	if ok != nil {
+		t.Errorf("test json meta get a error: %v", ok)
+	}
+
+	want := []MetaInfo{
+		MetaInfo{Name: "metaSymbol"},
+		MetaInfo{Name: "fdsa"},
+		MetaInfo{Name: "emptyTag"},
+	}
+
+	for i := 0; i < len(mi); i++ {
+		if r, w := mi[i], want[i]; r != w {
+			t.Errorf("want %v, get %v", w, r)
+		}
+	}
+}
