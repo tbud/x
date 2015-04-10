fb16195d491cf4b115d401b6b965b33f935f2622
peace0phmind <peace0phmind@gmail.com>
2015-04-10 21:44:38 +0800

    add dir copy

---
+++ io/ioutil/copydir.go
@@ -0,0 +1,86 @@
+package ioutil
+
+import (
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+type CopyFlags uint
+
+const (
+	FlagCopyHide CopyFlags = 1 << iota // if set this flag, whill copy hide dir ('.' prefix dir)
+)
+
+type CopyFilter func(dest, src string, srcInfo os.FileInfo) (skiped bool, err error)
+
+func Copy(destDir, srcDir string, copyFlags CopyFlags, copyFilter CopyFilter) (err error) {
+	if srcDir, err = filepath.Abs(srcDir); err != nil {
+		return err
+	}
+
+	if destDir, err = filepath.Abs(destDir); err != nil {
+		return err
+	}
+
+	var srcInfo os.FileInfo
+	if srcInfo, err = os.Stat(srcDir); err != nil {
+		if err = os.MkdirAll(destDir, srcInfo.Mode()); err != nil {
+			return err
+		}
+	}
+
+	srcDirLen := len(srcDir)
+
+	return filepath.Walk(srcDir, func(path string, info os.FileInfo, err error) (errr error) {
+		relSrcPath := strings.TrimLeft(path[srcDirLen:], string(os.PathSeparator))
+		destPath := filepath.Join(destDir, relSrcPath)
+
+		if strings.HasPrefix(relSrcPath, ".") {
+			if info.IsDir() && (copyFlags&FlagCopyHide != FlagCopyHide) {
+				return filepath.SkipDir
+			}
+		}
+
+		skiped := false
+		if copyFilter != nil {
+			if skiped, errr = copyFilter(destPath, path, info); errr != nil {
+				return errr
+			}
+		}
+
+		if !skiped {
+			if info.IsDir() {
+				errr = os.MkdirAll(destPath, info.Mode())
+				if !os.IsNotExist(errr) {
+					return errr
+				}
+				return nil
+			} else {
+				return copyFile(destPath, path)
+			}
+		}
+
+		return nil
+	})
+}
+
+func copyFile(destFile, srcFile string) (err error) {
+	var dst, src *os.File
+	if dst, err = os.Create(destFile); err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	if src, err = os.Open(srcFile); err != nil {
+		return err
+	}
+	defer src.Close()
+
+	if _, err = io.Copy(dst, src); err != nil {
+		return
+	}
+
+	return nil
+}
